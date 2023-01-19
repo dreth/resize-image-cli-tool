6124820d461fda480c675fb9f603aec9688a706c
dreth <almost_a_nightmare@pm.me>
2023-01-19 21:48:25 +0100

    fix issues regarding pathing

--- resize_images.py
+++ resize_images.py
@@ -1,6 +1,11 @@
 import argparse
 from PIL import Image
 import os
+from functools import reduce
+import operator
+
+# sep
+sep = os.path.sep
 
 # CLI argument parser
 parser = argparse.ArgumentParser(
@@ -8,11 +13,11 @@ parser = argparse.ArgumentParser(
 
 # input dir argument
 parser.add_argument("-i", "--input-dir", type=str,
-                    help="Input dir containing images. \nDefault: a subdir called images (images/)", default="images/")
+                    help="Input dir containing images. \nDefault: a subdir called images (images)", default="images")
 
 # output dir argument
 parser.add_argument("-o", "--output-dir", type=str,
-                    help="Output dir containing resized images. \nDefault: a subdir called resized_images (images_resized/)", default="images_resized/")
+                    help="Output dir containing resized images. \nDefault: a subdir called resized_images (images_resized)", default="images_resized")
 
 # factor argument
 parser.add_argument("-f", "--factor", type=float, default=1,
@@ -24,6 +29,14 @@ parser.add_argument("-v", "--verbose", default=False, help="Display files as the
 # parse arguments
 args = parser.parse_args()
 
+# output dir and input dir can't be the same
+if args.input_dir == args.output_dir:
+    raise ValueError("Input dir and output dir cannot be the same")
+
+# factor can't be negative
+if args.factor < 0:
+    raise ValueError("Factor cannot be negative")
+
 # run over file tree
 def resize_images(input_dir=args.input_dir, output_dir=args.output_dir, factor=args.factor, verbose=args.verbose):
     # create root images folder
@@ -32,35 +45,36 @@ def resize_images(input_dir=args.input_dir, output_dir=args.output_dir, factor=a
     # loop over files recursively
     for root, _, files in os.walk(input_dir):
         # save path
-        save_path = os.path.join(output_dir,
-                            root[len(input_dir):])
-        
+        save_path = os.path.join(output_dir, os.path.relpath(root, input_dir))
+
         # create folder if it doesn't exist
-        try_create_dir('/'.join(save_path.split('/')))
+        try_create_dir(save_path)
         
         for file in files:
             # show file if verbose
-            if verbose: print(f"{os.path.join(root, file)} --> {os.path.join('/'.join(save_path.split('/')), file)}")
+            if verbose: print(f"{os.path.join(root, file)} --> {os.path.join(save_path, file)}")
             
             # get the path of the file
             path = os.path.join(root, file)
             
-            # open the image
-            image = Image.open(path)
-            
+            try:
+                # open the image
+                image = Image.open(path)
+            except:
+                raise ValueError(f"Could not open {path} or file is not an image")
+                
             # resize the image
             image = image.resize(
                 (int(image.size[0]*factor), int(image.size[1]*factor)))
             
             # save the image
-            image.save(os.path.join(output_dir,
-                       path[len(input_dir):]))
+            image.save(os.path.join(save_path, file))
 
 # abstract this part of the code because i dont like how tryblocks look
 def try_create_dir(path):
     try:
         os.makedirs(path)
-    except:
+    except FileExistsError:
         pass
 
 # main
